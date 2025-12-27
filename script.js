7eefd10611f80f3ebd039e149c48bfd2587d9e9c
Halasz-Abel <abelqkacqkac@gmail.com>
2025-12-27 13:18:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Egyszerű gomb script
+document.getElementById("clickBtn").addEventListener("click", function() {
+    const msg = document.getElementById("msg");
+    msg.textContent = "Gratulálok, rákattintottál a gombra!";
+});
