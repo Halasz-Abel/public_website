3d91f4c6344d0c5350474b7573b04726d98e942a
Halasz-Abel <abelqkacqkac@gmail.com>
2025-12-27 14:12:49 +0100

    Remove click event listener from script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-// Egyszerű gomb script
-document.getElementById("clickBtn").addEventListener("click", function() {
-    const msg = document.getElementById("msg");
-    msg.textContent = "Gratulálok, rákattintottál a gombra!";
-});
+
